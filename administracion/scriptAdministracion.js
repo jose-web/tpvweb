206c69377d751804df2120f6a7f5d07c0de3081f
jose <delossantosramosjose@gmail.com>
2020-05-09 15:00:34 +0200

    Creada la interfaz funcional de administración y ahora te da la opción de elegir qué local deseas administrar ya que un usuario puede ser el encargado de varios locales a la vez y el botón de ir hacia atrás también funciona

--- administracion/scriptAdministracion.js
+++ administracion/scriptAdministracion.js
@@ -11,8 +11,31 @@ $(function () {
         }
     });
 
+    muestraLocalesEncargado();
+
     $("#botonIzquierda").on("click", function () {
-        location.href = "..";
+        switch (cambiaBotonIzquierda) {
+            case 0:
+                location.href = "..";
+                break;
+            case 1:
+                muestraLocalesEncargado();
+
+                $("h1").html("Locales que puedes administrar");
+                $("#opcionesLocal").hide();
+                $("#opciones").show();
+                break;
+
+        }
+
+    });
+
+    let cambiaBotonIzquierda = 0;
+    $(document).on("click", ".administraLocal", function () {
+        $("#opciones").hide();
+        $("#opcionesLocal").show();
+        $("h1").html("Administración");
+        cambiaBotonIzquierda = 1;
     });
 
     $("#cerrarSesion").on("click", function () {
@@ -27,3 +50,33 @@ $(function () {
 
 });
 
+
+function muestraLocalesEncargado() {
+    $.ajax({
+        url: "../REST/muestraLocalesEncargado",
+        method: 'get',
+        success: function (result) {
+            if (!result.locales)
+                location.href = "..";
+            else {
+                let numeroDeLocales = Object.keys(result.locales).length;
+                if (numeroDeLocales != 1) {
+                    $('#opciones').html("");
+                    for (let i = 0; i < numeroDeLocales; i++) {
+
+                        let id = result.locales[i].id;
+                        let nombreEmpresa = result.locales[i].nombreEmpresa;
+                        let nombreLocal = result.locales[i].nombreLocal;
+                        let direccion = result.locales[i].direccion;
+
+                        $('#opciones').append(`<div class="administraLocal" idLocal="${id}">
+                        <strong>${nombreEmpresa}</strong>
+                        <p>${nombreLocal}</p>
+                        <p>${direccion}</p>
+                        </div>`)
+                    }
+                }
+            }
+        }
+    });
+}

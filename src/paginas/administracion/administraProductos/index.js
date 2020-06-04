b0a9c55018e760a43e189fe3eb7bb7ab095fb572
jose <delossantosramosjose@gmail.com>
2020-06-04 20:52:56 +0200

    Se ha implementado la creación de productos

--- src/paginas/administracion/administraProductos/index.js
+++ src/paginas/administracion/administraProductos/index.js
@@ -19,12 +19,21 @@ export default class AdministracionLocal extends React.Component {
             arrayMenuProductos: "",
             arrayProductos: "",
             nombreCategoria: "",
-            abierto: false
-        };
+            abierto: false,
+            nombreProducto: "",
+            precioProducto: "",
+            descripcionProducto: "",
+            disponibilidadProducto: "",
+            codCategoria: ""
+        }
+
         this.irA = this.irA.bind(this)
         this.cambiaEstadoPopup = this.cambiaEstadoPopup.bind(this)
-        this.cambiaInputNombreCategoria = this.cambiaInputNombreCategoria.bind(this)
         this.nuevaCategoria = this.nuevaCategoria.bind(this)
+        this.popupNuevaCategoria = this.popupNuevaCategoria.bind(this)
+        this.popupNuevoProducto = this.popupNuevoProducto.bind(this)
+        this.cambiaEstado = this.cambiaEstado.bind(this)
+        this.nuevoProducto = this.nuevoProducto.bind(this)
     }
 
     componentDidMount() {
@@ -43,21 +52,50 @@ export default class AdministracionLocal extends React.Component {
                 let arrayMenuProductos = <ul key="menuProductos" id="menuProductos">{menuProductos}</ul>
 
                 for (let i = 0; i < Object.keys(res.categorias).length; i++) {
-                    // let id = res.categorias[i].codCategoria
+                    let id = res.categorias[i].codCategoria
                     let nombre = res.categorias[i].nombre
                     let categoria = res.categorias[i].codCategoria
-                    menuProductos.push(<li key={categoria + nombre} onClick={() => this.muestraCategoria(res.categorias[i].dentroCategoria)}>{nombre}</li>)
+                    menuProductos.push(<li key={categoria + nombre} onClick={() => this.muestraCategoria(id, res.categorias[i].dentroCategoria)}>{nombre}</li>)
                 }
-                menuProductos.push(<li key="mas" onClick={this.cambiaEstadoPopup}>+</li>)
+                menuProductos.push(<li key="mas" onClick={this.popupNuevaCategoria}>+</li>)
 
                 this.setState({ arrayMenuProductos })
 
-                this.muestraCategoria(res.categorias[0].dentroCategoria)
+                this.muestraCategoria(res.categorias[0].codCategoria, res.categorias[0].dentroCategoria)
 
             })
     }
 
-    muestraCategoria($array) {
+    popupNuevaCategoria() {
+        this.popup = <form id="formularioCambiaNombre" onSubmit={this.nuevaCategoria}>
+            <strong>Añadir una nueva categoría</strong>
+            <Input label="NOMBRE" cambia={($valor) => this.cambiaEstado($valor, "nombreCategoria")} />
+            <Button submit value="AÑADIR" />
+        </form>
+        this.cambiaEstadoPopup()
+    }
+
+    popupNuevoProducto() {
+        this.popup = <form id="formularioCambiaNombre" onSubmit={this.nuevoProducto}>
+            <strong>Añadir una nuevo producto</strong>
+            <Input label="NOMBRE" cambia={($valor) => this.cambiaEstado($valor, "nombreProducto")} />
+            <Input label="PRECIO" cambia={($valor) => this.cambiaEstado($valor, "precioProducto")} />
+            <Input label="DESCRIPCIÓN" cambia={($valor) => this.cambiaEstado($valor, "descripcionProducto")} />
+            <Input label="DISPONIBILIDAD" cambia={($valor) => this.cambiaEstado($valor, "disponibilidadProducto")} />
+            <Button submit value="AÑADIR" />
+        </form>
+        this.cambiaEstadoPopup()
+    }
+
+    cambiaEstado($valor, $nombre) {
+        let objaux = {}
+        objaux[$nombre] = $valor
+        this.setState(
+            objaux
+        )
+    }
+
+    muestraCategoria($codCategoria, $array) {
         let arrayProductos = []
 
         // arrayProductos.push(<article><strong><p>Añadir una categoría</p><p>+</p></strong></article>)
@@ -81,7 +119,7 @@ export default class AdministracionLocal extends React.Component {
                 className={($array[o].disponibilidad === "0" ? "opacidadAdministraCategoriaProducto " : "") + "administraCategoriaProducto"}
                 key={identificador}
                 tabIndex="0"
-                onClick={() => esCategoria ? this.muestraCategoria($array[o].dentroCategoria) : ""}
+                onClick={() => esCategoria ? this.muestraCategoria($array[o].codCategoria, $array[o].dentroCategoria) : ""}
             >
                 <div>
                     {$array[o].disponibilidad === "0" ? <del>{contenido}</del> : contenido}
@@ -89,7 +127,16 @@ export default class AdministracionLocal extends React.Component {
             </article>)
         }
 
-        this.setState({ arrayProductos })
+        if (Object.keys($array).length === 0) {
+            arrayProductos.push(<article>
+                <div>No hay productos</div>
+            </article>)
+        }
+
+        this.setState({
+            arrayProductos,
+            codCategoria: $codCategoria
+        })
     }
 
     irA($sitio) {
@@ -130,27 +177,45 @@ export default class AdministracionLocal extends React.Component {
             })
     }
 
-    cambiaInputNombreCategoria(evento) {
-        this.setState({
-            nombreCategoria: evento
-        })
+    nuevoProducto(evento) {
+        evento.preventDefault()
 
+        let url = global.url + 'crearProducto'
+
+        let usuario = JSON.parse(localStorage.getItem("usuario"))
+
+        let data = new FormData();
+        data.append('email', usuario.email)
+        data.append('pass', usuario.pass)
+        data.append('codLocal', sessionStorage.getItem("idLocal"))
+        data.append('codCategoria', this.state.codCategoria)
+        data.append('nombreProducto', this.state.nombreProducto)
+        data.append('precioProducto', this.state.precioProducto)
+        data.append('descripcionProducto', this.state.descripcionProducto)
+        data.append('disponibilidadProducto', this.state.disponibilidadProducto)
+
+        fetch(url, {
+            method: 'POST',
+            body: data,
+
+        }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(res => {
+                this.componentDidMount()
+                this.cambiaEstadoPopup()
+            })
     }
+
+    popup = ''
 
     render() {
         if (this.state.redireccionar !== "") {
             return <Redirect to={this.state.redireccionar} />
         }
 
-        let popup = <form id="formularioCambiaNombre" onSubmit={this.nuevaCategoria}>
-            <strong>Añadir una nueva categoría</strong>
-            <Input label="NOMBRE CATEGORÍA" cambia={this.cambiaInputNombreCategoria} />
-            <Button submit value="AÑADIR" />
-        </form>
-
         return (
             <>
-                <Popup contenido={popup} estado={this.state.abierto} cambiaEstadoPopup={this.cambiaEstadoPopup} />
+                <Popup contenido={this.popup} estado={this.state.abierto} cambiaEstadoPopup={this.cambiaEstadoPopup} />
                 <Menu estoyEn="administracion" />
                 <section id="seccionLocalAdministracion">
                     <h1>{sessionStorage.getItem("nombreLocal")} - Administración de productos</h1>
@@ -158,7 +223,7 @@ export default class AdministracionLocal extends React.Component {
                     {this.state.arrayProductos}
                 </section>
                 <BotonAbajo onClick={() => this.irA("/administracion")} />
-                <BotonAbajo derecha />
+                <BotonAbajo derecha onClick={this.popupNuevoProducto} />
             </>
         )
     }

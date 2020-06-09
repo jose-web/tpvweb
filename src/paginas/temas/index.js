9cd98870e4a277b2372f7851638a5b9d61c4db75
jose <delossantosramosjose@gmail.com>
2020-06-09 02:20:12 +0200

    Añadida la personalización de temas y funciona

--- src/paginas/temas/index.js
+++ src/paginas/temas/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './estilos.scss'
 import Menu from '../../componentes/menu'
+import Button from "../../componentes/button"
 import { Redirect } from "react-router-dom"
 
 export default class Locales extends React.Component {
@@ -9,7 +10,15 @@ export default class Locales extends React.Component {
         super(props);
         this.state = {
             arrayLocales: [],
-            redireccionar: false
+            redireccionar: false,
+            color1: '#262626',
+            color2: '#5E5E5E',
+            color3: '#B5B5B5',
+            color4: '#C4C4C4',
+            color5: '#EEEEEE',
+            color6: '#FFFFFF',
+            color7: '#AA3838',
+            color8: '#88BA69'
         };
     }
 
@@ -32,8 +41,12 @@ export default class Locales extends React.Component {
                 tema = `--color1: #177a03;--color2: #369B1A;--color3: #58AA3A;--color4: #8FEB65;--color5: #DEF9D4;`
                 break
 
+            case "personalizado":
+                tema = "--color1:" + this.state.color1 + ";--color2:" + this.state.color2 + ";--color3:" + this.state.color3 + ";--color4:" + this.state.color4 + ";--color5:" + this.state.color5 + ";--color6:" + this.state.color6 + ";--color7:" + this.state.color7 + ";--color8:" + this.state.color8 + ";"
+                break
+
             default:
-                tema = ""
+                tema = "porDefecto"
         }
 
         let usuario = JSON.parse(localStorage.getItem("usuario"))
@@ -56,6 +69,14 @@ export default class Locales extends React.Component {
                     document.body.style = tema
                 }
             });
+    }
+
+    cambiaEstado($valor, $nombre) {
+        let objaux = {}
+        objaux[$nombre] = $valor
+        this.setState(
+            objaux
+        )
     }
 
     render() {
@@ -81,6 +102,34 @@ export default class Locales extends React.Component {
                     <article onClick={() => this.cambiarTema("")} tabIndex="0">
                         Blanco y negro (Por defecto)
                     </article>
+
+                    <form id="personalizacionColores" onSubmit={(evento) => { evento.preventDefault(); this.cambiarTema("personalizado") }}>
+                        <h3>COLORES PERSONALIZADOS</h3>
+                        <label htmlFor="color1" id="contieneColor1" style={{ "--color1": this.state.color1 }} >Color1: {this.state.color1} <span /> </label>
+                        <input type="color" id="color1" onChange={($valor) => this.cambiaEstado($valor.target.value, "color1")} />
+
+                        <label htmlFor="color2" id="contieneColor2" style={{ "--color2": this.state.color2 }}>Color 2: {this.state.color2}<span /></label>
+                        <input type="color" id="color2" onChange={($valor) => this.cambiaEstado($valor.target.value, "color2")} />
+
+                        <label htmlFor="color3" id="contieneColor3" style={{ "--color3": this.state.color3 }}>Color 3: {this.state.color3}<span /></label>
+                        <input type="color" id="color3" onChange={($valor) => this.cambiaEstado($valor.target.value, "color3")} />
+
+                        <label htmlFor="color4" id="contieneColor4" style={{ "--color4": this.state.color4 }}>Color 4: {this.state.color4}<span /></label>
+                        <input type="color" id="color4" onChange={($valor) => this.cambiaEstado($valor.target.value, "color4")} />
+
+                        <label htmlFor="color5" id="contieneColor5" style={{ "--color5": this.state.color5 }}>Color 5: {this.state.color5}<span /></label>
+                        <input type="color" id="color5" onChange={($valor) => this.cambiaEstado($valor.target.value, "color5")} />
+
+                        <label htmlFor="color6" id="contieneColor6" style={{ "--color6": this.state.color6 }}>Color 6: {this.state.color6}<span /></label>
+                        <input type="color" id="color6" onChange={($valor) => this.cambiaEstado($valor.target.value, "color6")} />
+
+                        <label htmlFor="color7" id="contieneColor7" style={{ "--color7": this.state.color7 }}>Color 7: {this.state.color7}<span /></label>
+                        <input type="color" id="color7" onChange={($valor) => this.cambiaEstado($valor.target.value, "color7")} />
+
+                        <label htmlFor="color8" id="contieneColor8" style={{ "--color8": this.state.color8 }}>Color 8: {this.state.color8}<span /></label>
+                        <input type="color" id="color8" onChange={($valor) => this.cambiaEstado($valor.target.value, "color8")} />
+                        <Button value="UTILIZAR TEMA PERSONALIZADO" submit />
+                    </form>
                 </section>
             </>
         )

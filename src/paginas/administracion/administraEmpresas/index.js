7d1a3ac86b045605feec6be13effb5d702d3f3ea
jose <delossantosramosjose@gmail.com>
2020-09-10 10:54:20 +0200

    Se ha implementado el servicio "muestraMisEmpresas" en la página de administración de empresas

--- src/paginas/administracion/administraEmpresas/index.js
+++ src/paginas/administracion/administraEmpresas/index.js
@@ -4,14 +4,41 @@ import Menu from '../../../componentes/menu'
 export default class Empresas extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
 
-        };
+        }
     }
 
     componentDidMount() {
+        let url = global.url + 'muestraMisEmpresas'
 
+        let usuario = JSON.parse(localStorage.getItem("usuario"))
+
+        let data = new FormData()
+        data.append('email', usuario.email)
+        data.append('pass', usuario.pass)
+
+        fetch(url, {
+            method: 'POST',
+            body: data,
+
+        }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(res => {
+                let arrayEmpresas = []
+                console.log(res.empresas);
+                for (let i = 0; i < Object.keys(res.empresas).length; i++) {
+                    let empresa = res.empresas[i]
+                    arrayEmpresas.push(
+                        <article key={empresa.codEmpresa}>
+                            <p>{empresa.nombre}</p>
+                            <p>{empresa.nif}</p>
+                        </article>
+                    )
+                }
+                this.setState({ arrayEmpresas })
+            })
     }
 
     render() {
@@ -19,7 +46,11 @@ export default class Empresas extends React.Component {
             <>
                 <Menu />
                 <section id="locales">
-
+                    <h1>Empresas</h1>
+                    <article>
+                        <p>Crear una empresa</p>
+                    </article>
+                    {this.state.arrayEmpresas}
                 </section>
             </>
         )
